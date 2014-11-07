37d1b1f4afabe0ab584de1660e4123226db06c40
Martin Wicke <martin.wicke@gmail.com>
2014-11-07 15:40:34 -0800

    env test

---
+++ IntelliJ-plugin/tarski/src/TestEnvironment.scala
@@ -0,0 +1,26 @@
+package tarski
+
+import org.testng.annotations.Test
+import org.testng.AssertJUnit._
+import tarski.Base._
+import tarski.Environment._
+import tarski.Items._
+import tarski.Types._
+import tarski.Pretty._
+import tarski.Tokens._
+
+class TestEnvironment {
+
+  @Test
+  def testStaticShadowing() = {
+    val main = NormalClassItem("Main",LocalPkg,Nil,ObjectType,Nil)
+    val yf = StaticFieldItem("y",FloatType,main)
+    val f = StaticMethodItem("f",main,FloatType,List(ArrayType(IntType)))
+    val y = LocalVariableItem("y",ArrayType(DoubleType))
+    val scope = Map[NamedItem,Int]((LocalPkg,4),(main,3),(yf,2),(f,2),(y,1))
+    implicit val env = Env(List(main,f),scope)
+    assertEquals(tokens(y), List(IdentTok("y")))
+    assertEquals(tokens(yf), List(IdentTok("Main"),DotTok(), IdentTok("y")))
+  }
+
+}
