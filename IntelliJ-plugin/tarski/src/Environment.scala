f2492baea688b4921af0fffca1858ee2dee47b59
Geoffrey Irving <irving@naml.us>
2014-12-05 09:35:30 -0800

    Optimize memberIn

--- IntelliJ-plugin/tarski/src/Environment.scala
+++ IntelliJ-plugin/tarski/src/Environment.scala
@@ -189,9 +189,9 @@ object Environment {
 
   // Does a member belong to a type?
   def memberIn(f: Item, t: Type): Boolean = f match {
-    case f: Member => {
-      val p = f.parent
-      supers(t) exists (_.item == p)
+    case f: Member => f.parent match {
+      case p:ClassItem => isSubitem(t,p)
+      case _ => false
     }
     case _ => false
   }
