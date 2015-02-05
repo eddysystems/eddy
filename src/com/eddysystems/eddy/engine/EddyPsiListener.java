bc04f4395f797bf4e854fbd9051c4c5cc0dac417
martin <martin@eddy.systems>
2015-02-05 10:52:23 -0800

    reformat, kill eddy on beforeChildrenChanged

--- src/com/eddysystems/eddy/engine/EddyPsiListener.java
+++ src/com/eddysystems/eddy/engine/EddyPsiListener.java
@@ -11,21 +11,22 @@ public class EddyPsiListener implements PsiTreeChangeListener {
 
   // TODO: keep track of how many and which items we have changed, and request full or partial environment updates at appropriate frequencies
 
-  @Override
-  public void beforeChildAddition(@NotNull PsiTreeChangeEvent event) {
-    // for now, whenever the PSI changes, assume our current eddy process is out of date completely
-    EddyThread.kill();
-  }
+  // update byValue if
+  // - field changes type
+  // - new field
+  // - regularly
+
+  // update trie
+  // - new class/field/method name (either added or name changed)
+
+  @Override public void beforeChildAddition(@NotNull PsiTreeChangeEvent event) {}
+  @Override public void beforeChildRemoval(@NotNull PsiTreeChangeEvent event) {}
+  @Override public void beforeChildReplacement(@NotNull PsiTreeChangeEvent event) {}
+  @Override public void beforeChildMovement(@NotNull PsiTreeChangeEvent event) {}
 
   @Override
   public void childAdded(@NotNull PsiTreeChangeEvent event) {
     log("child " + event.getChild() + " added to " + event.getParent());
-  }
-
-  @Override
-  public void beforeChildRemoval(@NotNull PsiTreeChangeEvent event) {
-    // for now, whenever the PSI changes, assume our current eddy process is out of date completely
-    EddyThread.kill();
   }
 
   @Override
@@ -34,44 +35,37 @@ public class EddyPsiListener implements PsiTreeChangeListener {
   }
 
   @Override
-  public void beforeChildReplacement(@NotNull PsiTreeChangeEvent event) {
-    // for now, whenever the PSI changes, assume our current eddy process is out of date completely
-    EddyThread.kill();
-  }
-
-  @Override
   public void childReplaced(@NotNull PsiTreeChangeEvent event) {
     log("child " + event.getOldChild() + " in " + event.getParent() + " replaced with " + event.getNewChild());
   }
 
+  // Can ignore these because we'll also get detailed events like childReplaced
   @Override
   public void beforeChildrenChange(@NotNull PsiTreeChangeEvent event) {
     // for now, whenever the PSI changes, assume our current eddy process is out of date completely
     EddyThread.kill();
   }
 
+  // Can ignore these because we'll also get detailed events like childReplaced
   @Override
   public void childrenChanged(@NotNull PsiTreeChangeEvent event) {
     log("children of " + event.getParent() + " changed.");
   }
 
-  @Override
-  public void beforeChildMovement(@NotNull PsiTreeChangeEvent event) {
-    // for now, whenever the PSI changes, assume our current eddy process is out of date completely
-    EddyThread.kill();
-  }
-
+  // Probably doesn't matter, and we've never seen it anyways
   @Override
   public void childMoved(@NotNull PsiTreeChangeEvent event) {
     log("child " + event.getChild() + " moved from " + event.getOldParent() + " to " + event.getNewParent());
   }
 
+  // Doesn't matter
   @Override
   public void beforePropertyChange(@NotNull PsiTreeChangeEvent event) {
     // for now, whenever the PSI changes, assume our current eddy process is out of date completely
     EddyThread.kill();
   }
 
+  // Doesn't matter
   @Override
   public void propertyChanged(@NotNull PsiTreeChangeEvent event) {
     log("property " + event.getPropertyName() + " changed in " + event.getElement());
