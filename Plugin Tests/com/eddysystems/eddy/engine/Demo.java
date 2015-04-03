fc1b68f5c0a2ee40529aae7f22fe62c2fd31a6a1
martin <martin@eddy.systems>
2015-04-02 17:42:19 -0700

    added demo to test dir for easy access

---
+++ Plugin Tests/com/eddysystems/eddy/engine/Demo.java
@@ -0,0 +1,23 @@
+package com.eddysystems.eddy.engine;
+
+// need these
+
+import com.intellij.openapi.diagnostic.Logger;
+
+import java.util.ArrayList;
+
+public class Demo {
+  private Logger logger = Logger.getInstance("Demo");
+  private Object[] array;
+
+  public <T> T[] getAsArray() {
+    return (T[]) array;
+  }
+
+  private String n;
+
+  public ArrayList<Eddy> test(int n) {
+    return null;
+  }
+
+}
