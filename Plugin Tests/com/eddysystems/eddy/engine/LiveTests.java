b64a7f8876427a19779d59be733a63922bd7ff2b
martin <martin@eddy.systems>
2015-04-07 00:43:05 -0700

    add LiveTests.java

---
+++ Plugin Tests/com/eddysystems/eddy/engine/LiveTests.java
@@ -0,0 +1,27 @@
+package com.eddysystems.eddy.engine;
+
+import java.util.Set;
+
+/**
+ * Use this class to test a plugin deployment in each of the IntelliJ branches we have to test. Use it according
+ * to the comments, the delete everything you wrote to leave it in good condition for the next test.
+ */
+public class LiveTests {
+  void f(Set<Integer> ints) {
+    // type this:
+    // for i in ints:
+    // a hint should come up (only one option), accept it.
+
+    // type this:
+    // x = arraylist<Integer>
+    // a hint should come up (multiple options), accept it.
+
+    // type this:
+    // ints = null
+    // a hint should come up (auto-apply), accept it.
+
+    // type this:
+    // println ""
+    // press escape to hide the hint, then go through the lightbulb menu and accept eddy's suggestion.
+  }
+}
