7c8c82b1af42abf3ad2deeec8e7ac21cbdb93cd0
Geoffrey Irving <irving@naml.us>
2015-04-17 14:21:21 -0700

    Remove a particularly obnoxious line of debug code

--- src/com/eddysystems/eddy/engine/JavaEnvironment.java
+++ src/com/eddysystems/eddy/engine/JavaEnvironment.java
@@ -507,8 +507,6 @@ public class JavaEnvironment {
       final Map<PsiElement,Item> items = new HashMap<PsiElement,Item>();
       final Converter converter = new Converter(project, items);
 
-      ApplicationManager.getApplication().assertIsDispatchThread();
-
       // we could cache the result of addBase, but it doesn't seem like it's worth it.
       addBase(converter);
       
